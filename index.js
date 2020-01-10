b273d8e10b1f7d75d30d9618803e4c5846f2d8b6
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-01-10 22:06:57 +0000

    index.js added

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  array.forEach((item) => {
+    if (!Array.isArray(item)) {
+      sum += item;
+    } else {
+      sum += sumItems(item);
+    }
+  });
+  return sum;
 }
+
+//  console.log(sumItems([1, 2, [3, 5], 4])); //10
 
 module.exports = sumItems;
